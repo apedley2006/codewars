2319adea1e14af8422edd9e2cbc78e8323d12aa7
Andrew Pedley <apedley2006@gmail.com>
2022-06-10 07:33:22 -0400

    5th commit

--- js7kyu.js
+++ js7kyu.js
@@ -127,3 +127,14 @@ function mountainsOfHoiyama (width) {
   return (num + 1) * (2 * num ** 2 + 3 * num + 2)/2;
   }
 
+/* Vowel Count
+
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces. */
+
+function getCount(str) {
+  return (str.match(/[aeiou]/gi) || []).length;
+}
