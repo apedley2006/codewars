05deb27ce6b5beaeb17bf5efdaa75a71819be101
Andrew Pedley <apedley2006@gmail.com>
2022-06-14 09:11:35 -0400

    7th commit

--- js7kyu.js
+++ js7kyu.js
@@ -178,3 +178,20 @@ function barista(coffees){
   coffees.sort((a,b)=> a-b)
   return coffees.reduce((acc, cookTime, i) => acc + cookTime * (coffees.length-i) + 2*i, 0)
 }
+
+/* Boxlines
+
+Boxlines
+Given a X*Y*Z box built by arranging 1*1*1 unit boxes, write a function f(X,Y,Z) that returns the number of edges (hence, boxlines) of length 1 (both inside and outside of the box)
+
+Notes
+Adjacent unit boxes share the same edges, so a 2*1*1 box will have 20 edges, not 24 edges
+X,Y and Z are strictly positive, and can go as large as 2^16 - 1
+This is my first kata, so I hope every one will enjoy it <3 */
+
+function f(x,y,z){
+  //TODO:Your code here
+  return x * (y + 1) * (z + 1) +
+         y * (z + 1) * (x + 1) +
+         z * (x + 1) * (y + 1)
+}
