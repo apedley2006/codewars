d630517fb442f6d8ecdcde5555bbb2a19b0fe282
Andrew Pedley <apedley2006@gmail.com>
2022-06-15 09:11:38 -0400

    8th commit

--- js7kyu.js
+++ js7kyu.js
@@ -195,3 +195,20 @@ function f(x,y,z){
          y * (z + 1) * (x + 1) +
          z * (x + 1) * (y + 1)
 }
+
+/* Closure Counter
+
+Closure Counter
+Define the function counter that returns a function that returns an increasing value.
+The first value should be 1.
+You're going to have to use closures.
+Example:
+const newCounter = counter();
+newCounter() // 1
+newCounter() // 2
+Closure:
+A closure is the combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment). In other words, a closure gives you access to an outer function’s scope from an inner function. In JavaScript, closures are created every time a function is created, at function creation time.
+
+https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures */
+
+const counter = (n = 0) => () => (n += 1);
