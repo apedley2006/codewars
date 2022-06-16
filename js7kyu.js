ea98ca9dc3c8cfb94d94ad260b4e302f9642b7b7
Andrew Pedley <apedley2006@gmail.com>
2022-06-16 08:09:18 -0400

    9th commit

--- js7kyu.js
+++ js7kyu.js
@@ -212,3 +212,30 @@ A closure is the combination of a function bundled together (enclosed) with refe
 https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures */
 
 const counter = (n = 0) => () => (n += 1);
+
+/* Last Survivor
+
+You are given a string of letters and an array of numbers.
+The numbers indicate positions of letters that must be removed, in order, starting from the beginning of the array.
+After each removal the size of the string decreases (there is no empty space).
+Return the only letter left.
+
+Example:
+
+let str = "zbk", arr = [0, 1]
+    str = "bk", arr = [1]
+    str = "b", arr = []
+    return 'b'
+Notes
+The given string will never be empty.
+The length of the array is always one less than the length of the string.
+All numbers are valid.
+There can be duplicate letters and numbers. */
+
+function lastSurvivor(letters, coords) {
+  const arr = [...letters]
+  for(let i=0;i<coords.length;i++){
+    arr.splice(coords[i],1)
+  }
+  return arr.join``
+}
