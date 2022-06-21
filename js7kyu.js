95424f8ce237570da9991cef491db7172d2415fb
Andrew Pedley <apedley2006@gmail.com>
2022-06-21 08:10:41 -0400

    12thcommit

--- js7kyu.js
+++ js7kyu.js
@@ -310,3 +310,27 @@ function grid(N) {
   }
   return res.join('\n')
 }
+
+/* Previous multiple of three
+
+Given a positive integer n: 0 < n < 1e6, remove the last digit until you're left with a number that is a multiple of three.
+
+Return n if the input is already a multiple of three, and if no such number exists, return null, a similar empty value, or -1.
+
+Examples
+1      => null
+25     => null
+36     => 36
+1244   => 12
+952406 => 9 */
+
+const prevMultOfThree = n => {
+  //your solution
+  let str = n.toString().split('').reverse('').join('')
+  for(let i=0;i<str.length;i++){
+    if(str.slice(i).split('').reverse('').join('')*1%3===0){
+      return str.slice(i).split('').reverse('').join('')*1
+    }
+  }
+  return null
+}
