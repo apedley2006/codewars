a49a1706f366905208c937abe4f0b8265c6b38ff
Andrew Pedley <apedley2006@gmail.com>
2022-06-22 09:20:34 -0400

    13th commit

--- js7kyu.js
+++ js7kyu.js
@@ -334,3 +334,20 @@ const prevMultOfThree = n => {
   }
   return null
 }
+
+/* Find the Squares
+Problem
+Complete the function that takes an odd integer (0 < n < 1000000) which is the difference between two consecutive perfect squares, and return these squares as a string in the format "bigger-smaller"
+
+Examples
+9  -->  "25-16"
+5  -->  "9-4"
+7  -->  "16-9" */
+
+const findSquares = num => {
+  let ceil = Math.ceil(num / 2)
+  let floor = Math.floor(num / 2)
+  let max = ceil**2
+  let min = floor**2
+  return `${max}-${min}`;
+};
