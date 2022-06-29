60f0c584fda153fb55736a81682720507cd140d1
Andrew Pedley <apedley2006@gmail.com>
2022-06-29 08:52:29 -0400

    18th commit

--- js7kyu.js
+++ js7kyu.js
@@ -500,3 +500,50 @@ gridMap( x => x.toUpperCase() , xss )  =>  [['H','E','L','L','O'],['W','O','R','
 function gridMap(fn,a) {
   return a.map(v=>v.map(fn))
 }
+
+/* Tap Code Translation
+Tap Code Translation
+Tap code is a way to communicate using a series of taps and pauses for each letter. In this kata, we will use dots . for the taps and whitespaces for the pauses.
+
+The number of taps needed for each letter matches its coordinates in the following polybius square (note the c/k position). Then you "tap" the row, a pause, then the column. Each letter is separated from others with a pause too.
+
+   1  2  3  4  5
+1  A  B C\K D  E
+2  F  G  H  I  J
+3  L  M  N  O  P
+4  Q  R  S  T  U
+5  V  W  X  Y  Z
+Input:
+A lowercase string of a single word (no whitespaces or punctuation, only letters).
+
+Output:
+The encoded string as taps and pauses.
+
+Examples
+text = "dot"
+  "D" = (1, 4) = ". ...."
+  "O" = (3, 4) = "... ...."
+  "T" = (4, 4) = ".... ...."
+  
+output: ". .... ... .... .... ...."
+
+
+"example" -> ". ..... ..... ... . . ... .. ... ..... ... . . ....."
+"more"    -> "... .. ... .... .... .. . ....."
+Happy coding! */
+
+function tapCodeTranslation(text) {
+  let a = "abcdefghijlmnopqrstuvwxyz";
+  let res = [];
+  
+  text.split("").forEach(letter => {
+    if (letter == "k") letter = "c";
+    let i = a.indexOf(letter);
+    let row = Math.floor((i / 5) + 1);
+    let column = Math.floor(i % 5 + 1);
+
+    res.push(".".repeat(row), ".".repeat(column));
+  });
+  console.log(res.join(" "));
+  return res.join(" ");
+}
