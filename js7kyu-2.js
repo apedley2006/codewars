9ed7d1ad95c722f6816560e2b58641e036ee53af
Andrew Pedley <apedley2006@gmail.com>
2022-07-14 09:04:33 -0400

    10th commit

--- js7kyu-2.js
+++ js7kyu-2.js
@@ -211,3 +211,24 @@ function rotations(dieArray){
   let d = {1:6,2:5,3:4,4:3,5:2,6:1}
   return dieArray.length ? Math.min(...[...new Set(dieArray)].map(e=>dieArray.reduce((a,c)=>a+(e===c?0:d[c]===e?2:1),0))) : 0
 }
+
+/* Separate basic types
+Given: a sequence of different type of values (number, string, boolean). You should return an object with a separate properties for each of types presented in input. Each property should contain an array of corresponding values.
+
+keep order of values like in input array
+if type is not presented in input, no corresponding property are expected
+So, for this input:
+
+['a', 1, 2, false, 'b']
+expected output is:
+
+{
+  number: [1, 2],
+  string: ['a', 'b'],
+  boolean: [false]
+} */
+
+const separateTypes = input => input.reduce((a,b)=>{
+  const type = typeof b;
+  return {...a,[type]:input.filter(v=>typeof v === type)}
+},{})
