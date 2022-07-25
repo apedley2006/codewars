47eaebae967d21d2d5600af471402c7c55f824f0
Andrew Pedley <apedley2006@gmail.com>
2022-07-25 08:05:16 -0400

    17th commit

--- js7kyu-2.js
+++ js7kyu-2.js
@@ -418,3 +418,22 @@ function speedify(input) {
   [...input].map((c,i)=>arr[i+c.charCodeAt()-65]=c);
   return arr.join('').replace(/\s+$/gi,'');
 }
+
+/* Least Larger
+
+Task
+Given an array of numbers and an index, return either the index of the smallest number that is larger than the element at the given index, or -1 if there is no such index ( or, where applicable, Nothing or a similarly empty value ).
+
+Notes
+Multiple correct answers may be possible. In this case, return any one of them.
+The given index will be inside the given array.
+The given array will, therefore, never be empty.
+
+Example
+leastLarger( [4, 1, 3, 5, 6], 0 )  =>  3
+leastLarger( [4, 1, 3, 5, 6], 4 )  => -1 */
+
+function leastLarger(a,i) {
+  const arr = a.filter((v) => v > a[i]).sort((a, b) => a - b);
+  return a.lastIndexOf(arr[0]);
+}
