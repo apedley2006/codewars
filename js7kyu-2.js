fc010b388ea4c9b76d3492c197c7817221116056
Andrew Pedley <apedley2006@gmail.com>
2022-07-26 07:55:47 -0400

    18th commit

--- js7kyu-2.js
+++ js7kyu-2.js
@@ -437,3 +437,22 @@ function leastLarger(a,i) {
   const arr = a.filter((v) => v > a[i]).sort((a, b) => a - b);
   return a.lastIndexOf(arr[0]);
 }
+
+/* [Code Golf] Return Odd No Matter What
+Given the integer n return odd numbers as they are, but subtract 1 from even numbers.
+
+Note:
+
+Your solution should be 22 or less characters long.
+
+Examples
+Input  = 2
+Output = 1
+
+Input  = 13
+Output = 13
+
+Input  = 46
+Output = 45 */
+
+alwaysOdd=n=>n%2?n:n-1
