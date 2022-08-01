22b3e471db7228c49fa8a62b3046f9cb7eb16076
Andrew Pedley <apedley2006@gmail.com>
2022-08-01 07:45:54 -0400

    21st commit

--- js7kyu-3.js
+++ js7kyu-3.js
@@ -18,3 +18,16 @@ function solve(stones) {
       }
     } return count
   }
+
+/* Consecutive items
+
+You are given a list of unique integers arr, and two integers a and b. Your task is to find out whether or not a and b appear consecutively in arr, and return a boolean value (True if a and b are consecutive, False otherwise).
+
+It is guaranteed that a and b are both present in arr. */
+
+function consecutive(arr, a, b) {
+  // Your code here...
+  const indexA = arr.indexOf(a)
+  const indexB = arr.indexOf(b)
+  return Math.abs(indexA-indexB) === 1
+}
