5511cfc5766b8607a9e0ca0bb87eccf8ece07b1d
Andrew Pedley <apedley2006@gmail.com>
2022-08-11 08:24:12 -0400

    26th commit

--- js7kyu-3.js
+++ js7kyu-3.js
@@ -147,3 +147,25 @@ function baseFinder(seq){
   return new Set(seq.join``).size
 }
 
+/* Mirror, mirror, on the wall...
+
+Too long, didn't read
+You get a list of integers, and you have to write a function mirror that returns the "mirror" (or symmetric) version of this list: i.e. the middle element is the greatest, then the next greatest on both sides, then the next greatest, and so on...
+
+More info
+The list will always consist of integers in range -1000..1000 and will vary in size between 0 and 10000. Your function should not mutate the input array, and this will be tested (where applicable). Notice that the returned list will always be of odd size, since there will always be a definitive middle element.
+
+Examples
+[]  -->  []
+[1]  -->  [1]
+[2, 1]  -->  [1, 2, 1]
+[1, 3, 2]  -->  [1, 2, 3, 2, 1]
+[-8, 42, 18, 0, -16]  -->  [-16, -8, 0, 18, 42, 18, 0, -8, -16]
+[-3, 15, 8, -1, 7, -1] --> [-3, -1, -1, 7, 8, 15, 8, 7, -1, -1, -3] */
+
+function mirror(data) {
+  /* go ahead */
+  let sortA = data.slice().sort((a, b) => a - b)
+  let sortB = data.slice().sort((a, b) => b - a).slice(1)
+  return [...sortA, ...sortB]
+}
