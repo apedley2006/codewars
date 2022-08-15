1fc6631d1b6b79bd1d593eae5a00e0b6f5b04e3f
Andrew Pedley <apedley2006@gmail.com>
2022-08-15 09:58:44 -0400

    28th commit

--- js7kyu-3.js
+++ js7kyu-3.js
@@ -197,3 +197,37 @@ function overTheRoad(address, n){
   //code here
   return n * 2 - address + 1
 }
+
+/* 16+18=214
+
+For this kata you will have to forget how to add two numbers.
+
+It can be best explained using the following meme:
+
+Dayane Rivas adding up a sum while competing in the Guatemalan television show "Combate" in May 2016
+
+In simple terms, our method does not like the principle of carrying over numbers and just writes down every number it calculates :-)
+
+You may assume both integers are positive integers. */
+
+function add(num1, num2) {
+  num1 = num1.toString()
+  num2 = num2.toString()
+  let arr = []
+  const l1 = num1.length-1
+  const l2 = num2.length-1
+  for (let i=0;;i++){
+    if (num1[l1-i]&&num2[l2-i]){ 
+      arr.push(parseInt(num1[l1-i])+parseInt(num2[l2-i]))
+    }
+    else if (num1[l1-i]){
+      arr.push(num1[l1-i])
+    }
+    else if (num2[l2-i]){
+      arr.push(num2[l2-i])
+    } else {
+      break
+    }
+  }
+  return arr.reverse().join('')*1
+}
