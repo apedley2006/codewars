0fbedbc455ebb6ea872ae86e9ee02060719ff61c
Andrew Pedley <apedley2006@gmail.com>
2022-09-06 07:34:47 -0400

    7th commit

--- strings.js
+++ strings.js
@@ -104,3 +104,23 @@ function well(x){
   const count = x.reduce((s, v) => s + (v == 'good'), 0)
   return count ? count > 2 ? 'I smell a series!' : 'Publish!' : 'Fail!'
 }
+
+/* The Wide-Mouthed frog!
+
+The wide-mouth frog is particularly interested in the eating habits of other creatures.
+
+He just can't stop asking the creatures he encounters what they like to eat. But, then he meets the alligator who just LOVES to eat wide-mouthed frogs!
+
+When he meets the alligator, it then makes a tiny mouth.
+
+Your goal in this kata is to create complete the mouth_size method this method takes one argument animal which corresponds to the animal encountered by the frog. If this one is an alligator (case-insensitive) return small otherwise return wide */
+
+function mouthSize(animal) {
+  // code here
+  animal = animal.toLowerCase()
+  if (animal == "alligator"){
+    return "small"
+  } else {
+    return "wide"
+  }
+}
