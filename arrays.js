73ece129ca2ad6ab6af08a2f47666c49d7b66051
Andrew Pedley <apedley2006@gmail.com>
2022-09-09 07:33:06 -0400

    3rd commit

--- arrays.js
+++ arrays.js
@@ -35,3 +35,15 @@ function duckDuckGoose(players, goose) {
   // ...
   return players[(goose-1)%players.length].name
 }
+
+/* Beginner - Lost Without a Map
+
+Given an array of integers, return a new array with each value doubled.
+
+For example:
+
+[1, 2, 3] --> [2, 4, 6] */
+
+function maps(x){
+  return x.map( i => i *2)
+}
