30e3cdc08017a1a0642136b3f7934be5d024a678
Andrew Pedley <apedley2006@gmail.com>
2022-09-12 07:38:59 -0400

    4th commit

--- arrays.js
+++ arrays.js
@@ -47,3 +47,13 @@ For example:
 function maps(x){
   return x.map( i => i *2)
 }
+
+/* Beginner - Reduce but Grow
+
+Given a non-empty array of integers, return the result of multiplying the values together in order. Example:
+
+[1, 2, 3, 4] => 1 * 2 * 3 * 4 = 24 */
+
+function grow(x){
+  return x.reduce((i, a) => i * a)
+}
