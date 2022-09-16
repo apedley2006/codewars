e79112f802add087892352df7eb1a48ced81408f
Andrew Pedley <apedley2006@gmail.com>
2022-09-16 07:24:09 -0400

    7th commit

--- arrays.js
+++ arrays.js
@@ -105,3 +105,14 @@ function gooseFilter (birds) {
   
   return birds.filter(i => !geese.includes(i))
 }
+
+/* Sum Mixed Array
+
+Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+Return your answer as a number. */
+
+function sumMix(x){
+  let num = x.map(Number)
+  return num.reduce((x, y) => x + y)
+}
