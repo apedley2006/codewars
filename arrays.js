b6cbcd9ff465da71a86b31814db8c94dcb5f13b8
Andrew Pedley <apedley2006@gmail.com>
2022-09-21 07:33:38 -0400

    10th commit

--- arrays.js
+++ arrays.js
@@ -137,3 +137,20 @@ function noSpace(x){
 function noSpace(x){
   return x.split(' ').join('')
 }
+
+/* Abbreviate a Two Word Name
+
+Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+The output should be two capital letters with a dot separating them.
+
+It should look like this:
+
+Sam Harris => S.H
+
+patrick feeney => P.F */
+
+function abbrevName(name){
+  // code away
+    return name.split(' ').map((fili) => fili[0].toUpperCase()).join('.')  
+  }
