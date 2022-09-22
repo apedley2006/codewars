12dd8245c538c77edcf0a2a4f336749deb484bb2
Andrew Pedley <apedley2006@gmail.com>
2022-09-22 07:40:31 -0400

    11th commit

--- arrays.js
+++ arrays.js
@@ -154,3 +154,15 @@ function abbrevName(name){
   // code away
     return name.split(' ').map((fili) => fili[0].toUpperCase()).join('.')  
   }
+
+/* Is there a vowel in there?
+
+Given an array of numbers, check if any of the numbers are the character codes for lower case vowels (a, e, i, o, u).
+
+If they are, change the array value to a string of that vowel.
+
+Return the resulting array. */
+
+function isVow(a){
+  return  a.map(charCode => /[aeiou]/.test(String.fromCharCode(charCode)) ? String.fromCharCode(charCode) : charCode)
+  }
