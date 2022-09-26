27a37c79961f56d56372523fbdbf7a83f536272c
Andrew Pedley <apedley2006@gmail.com>
2022-09-26 07:23:01 -0400

    12th commit

--- arrays.js
+++ arrays.js
@@ -166,3 +166,17 @@ Return the resulting array. */
 function isVow(a){
   return  a.map(charCode => /[aeiou]/.test(String.fromCharCode(charCode)) ? String.fromCharCode(charCode) : charCode)
   }
+
+/* You only need one - Beginner
+
+You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+Array can contain numbers or strings. X can be either.
+
+Return true if the array contains the value, false if not. */
+
+function check(a, x) {
+  // your code here
+  
+  return a.includes(x)
+}
