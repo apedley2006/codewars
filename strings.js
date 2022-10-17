d5b2925aa117d483d4edd931e451212437dede91
Andrew Pedley <apedley2006@gmail.com>
2022-10-17 08:50:46 -0400

    10-17-22

--- strings.js
+++ strings.js
@@ -170,3 +170,19 @@ function stringClean(s){
   // Function will return the cleaned string
   return s.replace(/\d/g, "")
 }
+
+/* Reversing Words in a String
+
+You need to write a function that reverses the words in a given string. A word can also fit an empty string. If this is not clear enough, here are some examples:
+
+As the input may have trailing spaces, you will also need to ignore unneccesary whitespace.
+
+Example (Input --> Output)
+
+"Hello World" --> "World Hello"
+"Hi There." --> "There. Hi" */
+
+function reverse(string){
+  //your code here
+  return string.split(" ").reverse().join(" ")
+}
