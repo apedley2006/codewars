4f730dfedc69099304ba5c935a2de3a04d464c19
Andrew Pedley <apedley2006@gmail.com>
2022-10-18 07:25:23 -0400

    10-18-22

--- strings.js
+++ strings.js
@@ -186,3 +186,15 @@ function reverse(string){
   //your code here
   return string.split(" ").reverse().join(" ")
 }
+
+/* Get number from string
+
+Write a function which removes from string all non-digit characters and parse the remaining to number. E.g: "hell5o wor6ld" -> 56
+
+Function:
+
+getNumberFromString(s) */
+
+function getNumberFromString(s) {
+  return Number(s.match(/\d/g).join(''))
+}
