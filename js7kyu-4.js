3de935f07a477cc13d2819cbf82d836d42a1c49d
Andrew Pedley <apedley2006@gmail.com>
2022-12-20 07:34:29 -0500

    12-20-22

--- js7kyu-4.js
+++ js7kyu-4.js
@@ -7,3 +7,17 @@ Example:
 keysAndValues({a: 1, b: 2, c: 3}) // should return [['a', 'b', 'c'], [1, 2, 3]] */
 
 const keysAndValues = data => [Object.keys(data), Object.values(data)];
+
+/* Disemvowel Trolls
+
+Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!". */
+
+function disemvowel(str) {
+    return str.replace(/[aeuio]/gi, "")
+  }
