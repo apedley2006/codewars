2b2e134dcae88d55d4eb6fe83a56aa7e5d841753
Andrew Pedley <apedley2006@gmail.com>
2022-12-22 07:28:07 -0500

    12-22-22

--- js7kyu-4.js
+++ js7kyu-4.js
@@ -21,3 +21,15 @@ For example, the string "This website is for losers LOL!" would become "Ths wbst
 function disemvowel(str) {
     return str.replace(/[aeuio]/gi, "")
   }
+
+/* Square Every Digit
+
+Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
+
+Note: The function accepts an integer and returns an integer */
+
+function squareDigits(num){
+  return +(num + "").split("").map(value => value * value).join("")
+}
