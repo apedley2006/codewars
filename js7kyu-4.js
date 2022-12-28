c053451b573828b2a4ff509d2cf3474bd6a0de44
Andrew Pedley <apedley2006@gmail.com>
2022-12-28 07:26:33 -0500

    12-28-22

--- js7kyu-4.js
+++ js7kyu-4.js
@@ -53,3 +53,20 @@ function highAndLow(numbers){
   let nums = numbers.split(" ")
   return `${Math.max(...nums)} ${Math.min(...nums)}`
 }
+
+/* Descending Order
+
+Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+Examples:
+Input: 42145 Output: 54421
+
+Input: 145263 Output: 654321
+
+Input: 123456789 Output: 987654321 */
+
+function descendingOrder(n){
+  // have to conver it to a string at first, could also use +n.toString
+  
+  return +(n + "").split("").sort().reverse().join("")
+}
