186bfc4df79feea22baee2f44f22daa84435e404
Andrew Pedley <apedley2006@gmail.com>
2023-01-04 07:36:19 -0500

    1-4-23

--- js7kyu-4.js
+++ js7kyu-4.js
@@ -101,3 +101,18 @@ function getMiddle(s) {
     return s.slice(s.length / 2, s.length / 2 + 1);
   }
 }
+
+/* Mumbling
+
+This time no story, no theory. The examples below show you how to write function accum:
+
+Examples:
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+The parameter of accum is a string which includes only letters from a..z and A..Z. */
+
+function accum(s) {
+	// your code
+  return s.split("").map((c, i) => c.toUpperCase() + c.toLowerCase().repeat(i)).join("-")
+}
