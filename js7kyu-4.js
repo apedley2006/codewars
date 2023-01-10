0ab74fbb0f0f54462a1891953cd203060023fd01
Andrew Pedley <apedley2006@gmail.com>
2023-01-10 07:39:20 -0500

    1-10-23

--- js7kyu-4.js
+++ js7kyu-4.js
@@ -130,3 +130,20 @@ function filter_list(l) {
   // Return a new array with the strings filtered out
   return l.filter(i => typeof i == "number")
 }
+
+/* Isograms
+
+An isogram is a word that has no repeating letters, consecutive or non-consecutive. Implement a function that determines whether a string that contains only letters is an isogram. Assume the empty string is an isogram. Ignore letter case.
+
+Example: (Input --> Output)
+
+"Dermatoglyphics" --> true "aba" --> false "moOse" --> false (ignore letter case)
+
+isIsogram "Dermatoglyphics" = true
+isIsogram "moose" = false
+isIsogram "aba" = false */
+
+function isIsogram(str){
+  //...
+  return !str.match(/([a-z]).*\1/i);
+}
