e3138616764c471c29f06e1c58e6092a27974335
Andrew Pedley <apedley2006@gmail.com>
2023-01-11 13:05:19 -0500

    1-11-23

--- js7kyu-4.js
+++ js7kyu-4.js
@@ -147,3 +147,23 @@ function isIsogram(str){
   //...
   return !str.match(/([a-z]).*\1/i);
 }
+
+/* Exes and Ohs
+
+Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+Examples input/output:
+
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+XO("zzoo") => false */
+
+function XO(str) {
+  //code here
+
+let x = str.match(/x/gi)
+let o = str.match(/o/gi)
+return (x && x.length) === (o && o.length)
+}
