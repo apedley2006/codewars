361741c6c19037f01cde9b3461b165195cae3a6a
Andrew Pedley <apedley2006@gmail.com>
2023-01-12 08:12:37 -0500

    1-12-23

--- js7kyu-4.js
+++ js7kyu-4.js
@@ -167,3 +167,13 @@ let x = str.match(/x/gi)
 let o = str.match(/o/gi)
 return (x && x.length) === (o && o.length)
 }
+
+/* Shortest Word
+
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types. */
+
+function findShort(s) {
+  return Math.min(...s.split(" ").map(s => s.length))
+}
