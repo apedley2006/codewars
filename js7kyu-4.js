7546da6f5bd69fa120b39a6c519dd961ae9c0405
Andrew Pedley <apedley2006@gmail.com>
2023-01-19 08:13:07 -0500

    Sum of two lowest positive integers

--- js7kyu-4.js
+++ js7kyu-4.js
@@ -239,3 +239,17 @@ Examples
 function maskify(cc) {
   return cc.slice(0, -4).replace(/\w/g, "#") + cc.slice(-4)
 }
+
+/* Sum of two lowest positive integers
+
+Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+For example, when an array is passed like [19, 5, 42, 2, 77], the output should be 7.
+
+[10, 343445353, 3453445, 3453545353453] should return 3453455. */
+
+function sumTwoSmallestNumbers(numbers) {  
+  //Code here
+  let smallestSum = numbers.sort((a, b) => a - b)
+  return smallestSum[0] + smallestSum[1]
+}
