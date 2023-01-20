8a3100805e9f3eaeab9ffa430b6677f2e6031704
Andrew Pedley <apedley2006@gmail.com>
2023-01-20 09:13:45 -0500

    Beginner Series #3 Sum of Numbers

--- js7kyu-4.js
+++ js7kyu-4.js
@@ -253,3 +253,24 @@ function sumTwoSmallestNumbers(numbers) {
   let smallestSum = numbers.sort((a, b) => a - b)
   return smallestSum[0] + smallestSum[1]
 }
+
+/* Beginner Series #3 Sum of Numbers
+
+Given two integers a and b, which can be positive or negative, find the sum of all the integers between and including them and return it. If the two numbers are equal return a or b.
+
+Note: a and b are not ordered!
+
+Examples (a, b) --> output (explanation)
+(1, 0) --> 1 (1 + 0 = 1)
+(1, 2) --> 3 (1 + 2 = 3)
+(0, 1) --> 1 (0 + 1 = 1)
+(1, 1) --> 1 (1 since both are same)
+(-1, 0) --> -1 (-1 + 0 = -1)
+(-1, 2) --> 2 (-1 + 0 + 1 + 2 = 2) */
+
+function getSum(a, b) {
+  //Good luck!
+  let min = Math.min(a, b)
+  let max = Math.max(a, b)
+  return ((max - min + 1) * (min + max)) / 2
+}
