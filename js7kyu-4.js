3a0e304109281d47cee091dfc0bc36c187a52349
Andrew Pedley <apedley2006@gmail.com>
2023-01-23 08:36:33 -0500

    Two to One

--- js7kyu-4.js
+++ js7kyu-4.js
@@ -274,3 +274,21 @@ function getSum(a, b) {
   let max = Math.max(a, b)
   return ((max - min + 1) * (min + max)) / 2
 }
+
+/* Two to One
+
+Take 2 strings s1 and s2 including only letters from a to z. Return a new sorted string, the longest possible, containing distinct letters - each taken only once - coming from s1 or s2.
+
+Examples:
+a = "xyaabbbccccdefww"
+b = "xxxxyyyyabklmopq"
+longest(a, b) -> "abcdefklmopqwxy"
+
+a = "abcdefghijklmnopqrstuvwxyz"
+longest(a, a) -> "abcdefghijklmnopqrstuvwxyz" */
+
+function longest(s1, s2) {
+  // your code
+  
+  return [...new Set(s1 + s2)].sort().join("")
+}
