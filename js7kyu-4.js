5e8cde39e4356e58cebf455e483dc89364ae68c7
Andrew Pedley <apedley2006@gmail.com>
2023-02-06 07:59:58 -0500

    String ends with?

--- js7kyu-4.js
+++ js7kyu-4.js
@@ -425,3 +425,18 @@ const binaryArrayToNumber = arr => {
   
   return parseInt(arr.join(""), 2)
 };
+
+/* String ends with?
+
+Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+Examples:
+
+solution('abc', 'bc') // returns true
+solution('abc', 'd') // returns false */
+
+function solution(str, ending){
+  // TODO: complete
+  
+  return new RegExp(ending + "$", "i").test(str)
+}
