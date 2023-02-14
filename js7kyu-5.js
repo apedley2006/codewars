58529ccc6e12af318e7a11cec4032e83b1f407d7
Andrew Pedley <apedley2006@gmail.com>
2023-02-14 07:30:26 -0500

    Remove the minimum

--- js7kyu-5.js
+++ js7kyu-5.js
@@ -12,9 +12,9 @@ function reverseWords(str) {
     return str.split("").reverse().join("").split(" ").reverse().join(" ")
   }
 
-  /* Sum of the first nth term of Series
+/* Sum of the first nth term of Series
 
-  Task:
+Task:
 Your task is to write a function which returns the sum of following series upto nth term(parameter).
 
 Series: 1 + 1/4 + 1/7 + 1/10 + 1/13 + 1/16 +...
@@ -39,4 +39,26 @@ function SeriesSum(n)
   }
 
   return s.toFixed(2)
+}
+
+/* Remove the minimum
+
+The museum of incredible dull things
+The museum of incredible dull things wants to get rid of some exhibitions. Miriam, the interior architect, comes up with a plan to remove the most boring exhibitions. She gives them a rating, and then removes the one with the lowest rating.
+
+However, just as she finished rating all exhibitions, she's off to an important fair, so she asks you to write a program that tells her the ratings of the items after one removed the lowest one. Fair enough.
+
+Task
+Given an array of integers, remove the smallest value. Do not mutate the original array/list. If there are multiple elements with the same value, remove the one with a lower index. If you get an empty array/list, return an empty array/list.
+
+Don't change the order of the elements that are left.
+
+Examples
+* Input: [1,2,3,4,5], output = [2,3,4,5]
+* Input: [5,3,2,1,4], output = [5,3,2,4]
+* Input: [2,2,1,2,1], output = [2,2,2,1] */
+
+function removeSmallest(numbers) {
+  let min = numbers.indexOf(Math.min(...numbers))
+  return [...numbers.slice(0, min), ...numbers.slice(min + 1)]
 }
