211fbf41230202d67db419a1464c18640a792a1f
Andrew Pedley <apedley2006@gmail.com>
2023-03-01 07:19:44 -0500

    Sum of a sequence

--- js7kyu-5.js
+++ js7kyu-5.js
@@ -245,3 +245,24 @@ gimme([5, 10, 14]) => 1
 function gimme (triplet) {
   return [...triplet].indexOf(triplet.sort((a, b) => a - b)[1])
 }
+
+/* Sum of a sequence
+
+Your task is to make function, which returns the sum of a sequence of integers.
+
+The sequence is defined by 3 non-negative values: begin, end, step (inclusive).
+
+If begin value is greater than the end, function should returns 0
+
+Examples
+
+2,2,2 --> 2
+2,6,2 --> 12 (2 + 4 + 6)
+1,5,1 --> 15 (1 + 2 + 3 + 4 + 5)
+1,5,3  --> 5 (1 + 4) */
+
+const sequenceSum = (begin, end, step) =>
+  // May the Force be with you
+  begin > end ? 0 : begin + sequenceSum(begin + step, end, step)
+
+  
