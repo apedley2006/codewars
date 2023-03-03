5d332f26c19c4142f3f9f51ce030d6ec23fcfdf2
Andrew Pedley <apedley2006@gmail.com>
2023-03-03 07:39:56 -0500

    Round up to the next multiple of 5

--- js7kyu-5.js
+++ js7kyu-5.js
@@ -283,3 +283,28 @@ var isAnagram = function(test, original) {
   return test.toLowerCase().split("").sort().join("") === original.toLowerCase().split("").sort().join("")
 };
 
+/* Round up to the next multiple of 5
+
+Given an integer as input, can you round it to the next (meaning, "greater than or equal") multiple of 5?
+
+Examples:
+
+input:    output:
+0    ->   0
+2    ->   5
+3    ->   5
+12   ->   15
+21   ->   25
+30   ->   30
+-2   ->   0
+-5   ->   -5
+etc.
+Input may be any positive or negative integer (including 0).
+
+You can assume that all inputs are valid integers. */
+
+function roundToNext5(n){
+  // ...
+  return Math.ceil(n / 5) * 5
+}
+
