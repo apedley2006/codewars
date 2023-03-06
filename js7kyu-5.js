46c52847395b1182d8f7416d4b9ae7cdf2b8ad8a
Andrew Pedley <apedley2006@gmail.com>
2023-03-06 07:18:25 -0500

    Remove anchor from URL

--- js7kyu-5.js
+++ js7kyu-5.js
@@ -308,3 +308,15 @@ function roundToNext5(n){
   return Math.ceil(n / 5) * 5
 }
 
+/* Remove anchor from URL
+
+Complete the function/method so that it returns the url with anything after the anchor (#) removed.
+
+Examples
+"www.codewars.com#about" --> "www.codewars.com"
+"www.codewars.com?page=1" -->"www.codewars.com?page=1" */
+
+function removeUrlAnchor(url){
+  // TODO: complete
+  return url.replace(/#.*/, "")
+}
