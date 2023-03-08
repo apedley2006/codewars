1ae098299b0f34a32f5d21cf3bbe5176b1322553
Andrew Pedley <apedley2006@gmail.com>
2023-03-08 07:22:03 -0500

    Small enough? - Beginner

--- js7kyu-5.js
+++ js7kyu-5.js
@@ -333,3 +333,14 @@ solution(null); // should return [] */
 function solution(nums){
   return (nums || []).sort((a, b) => a - b)
 }
+
+/* Small enough? - Beginner
+
+You will be given an array and a limit value. You must check that all values in the array are below or equal to the limit value. If they are, return true. Else, return false.
+
+You can assume all values in the array are numbers. */
+
+function smallEnough(a, limit){
+  return a.every(v => v <= limit)
+}
+
