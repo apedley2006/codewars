e619e1541d97e41070bbdf16dbac9badcc75abd2
Andrew Pedley <apedley2006@gmail.com>
2023-03-13 07:35:38 -0400

    Simple Fun #176: Reverse Letter

--- js7kyu-5.js
+++ js7kyu-5.js
@@ -393,3 +393,26 @@ function declareWinner(fighter1, fighter2, firstAttacker) {
   const f2 = Math.ceil(fighter2.health / fighter1.damagePerAttack);
   return f1 < f2 ? fighter2.name : f2 < f1 ? fighter1.name : firstAttacker
 }
+
+/* Simple Fun #176: Reverse Letter
+
+Task
+Given a string str, reverse it and omit all non-alphabetic characters.
+
+Example
+For str = "krishan", the output should be "nahsirk".
+
+For str = "ultr53o?n", the output should be "nortlu".
+
+Input/Output
+[input] string str
+A string consists of lowercase latin letters, digits and symbols.
+
+[output] a string */
+
+reverseLetter = s =>
+  s
+    .replace(/[^a-z]/gi, "")
+    .split("")
+    .reverse()
+    .join("");
