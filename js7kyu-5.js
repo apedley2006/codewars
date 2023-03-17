a19ab28dfb003b596d0fce2847deb08bd7eb39a8
Andrew Pedley <apedley2006@gmail.com>
2023-03-17 08:23:17 -0400

    Form The Minimum

--- js7kyu-5.js
+++ js7kyu-5.js
@@ -478,3 +478,28 @@ Let's assume that all numbers in the input will be integer values.*/
 function sumDigits(number) {
   return Math.abs(number).toString().split("").reduce((total, next) => total + Number(next), 0);
 }
+
+/* Form The Minimum
+
+Task
+Given a list of digits, return the smallest number that could be formed from these digits, using the digits only once (ignore duplicates).
+
+Notes:
+Only positive integers will be passed to the function (> 0 ), no negatives or zeros.
+Input >> Output Examples
+minValue ({1, 3, 1})  ==> return (13)
+Explanation:
+(13) is the minimum number could be formed from {1, 3, 1} , Without duplications
+
+minValue({5, 7, 5, 9, 7})  ==> return (579)
+Explanation:
+(579) is the minimum number could be formed from {5, 7, 5, 9, 7} , Without duplications
+
+minValue({1, 9, 3, 1, 7, 4, 6, 6, 7}) return  ==> (134679)
+Explanation:
+(134679) is the minimum number could be formed from {1, 9, 3, 1, 7, 4, 6, 6, 7} , Without duplications */
+
+function minValue(values){
+  //your code here
+  return +Array.from(new Set(values)).sort((a, b) => a - b).join("")
+}
