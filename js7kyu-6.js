93afe1bc89090b65fc03c04cc4b958345cf6b9bf
Andrew Pedley <apedley2006@gmail.com>
2023-04-06 07:30:42 -0400

    JavaScript Array Filter

--- js7kyu-6.js
+++ js7kyu-6.js
@@ -166,3 +166,18 @@ function removeDuplicateWords (s) {
   // your perfect code...
   return [...new Set(s.split(" "))].join(" ")
 }
+
+/* JavaScript Array Filter
+
+JavaScript Arrays support a filter function (starting in JavaScript 1.6). Use the filter functionality to complete the function given.
+
+The solution would work like the following:
+
+getEvenNumbers([2,4,5,6]) // should == [2,4,6] */
+
+function getEvenNumbers(numbersArray){
+  // filter out the odd numbers
+  return numbersArray.filter(value => {
+    if (value % 2 == 0) return value;
+  });
+}
