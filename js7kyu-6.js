5f3212313ab4fa6bdec96282e94730bad2131f6c
Andrew Pedley <apedley2006@gmail.com>
2023-04-11 07:08:22 -0400

    Sum of angles

--- js7kyu-6.js
+++ js7kyu-6.js
@@ -212,3 +212,12 @@ function countDevelopers(list) {
   
   return list.filter(x => x.continent == "Europe" && x.language == "JavaScript").length
 }
+
+/* Sum of angles
+
+Find the total sum of internal angles (in degrees) in an n-sided simple polygon. N will be greater than 2. */
+
+function angle(n) {
+  //code here
+  return (n - 2) * 180
+}
