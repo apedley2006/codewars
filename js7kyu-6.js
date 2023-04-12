bf8d3fb8c0e7e0695bd40b9537d23e61511e1300
Andrew Pedley <apedley2006@gmail.com>
2023-04-12 07:10:17 -0400

    Largest pair sum in array

--- js7kyu-6.js
+++ js7kyu-6.js
@@ -221,3 +221,18 @@ function angle(n) {
   //code here
   return (n - 2) * 180
 }
+
+/* Largest pair sum in array
+
+Given a sequence of numbers, find the largest pair sum in the sequence.
+
+For example
+
+[10, 14, 2, 23, 19] -->  42 (= 23 + 19)
+[99, 2, 2, 23, 19]  --> 122 (= 99 + 23)
+Input sequence contains minimum two elements and every element is an integer. */
+
+function largestPairSum (numbers) {
+  //TODO: Write your Code here
+  return numbers.sort((a, b) => a - b).slice(-2).reduce((a, b) => a + b, 0)
+}
