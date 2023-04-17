1a6307a50f4216fd51bddaefa5e652c9c49b1cc1
Andrew Pedley <apedley2006@gmail.com>
2023-04-17 07:31:43 -0400

    Sorted? yes? no? how?

--- js7kyu-6.js
+++ js7kyu-6.js
@@ -254,3 +254,20 @@ The input will always be made of only lowercase letters and will never be empty.
 const wordsToMarks = s =>
   [...s].reduce((res, c) => (res += c.charCodeAt() - 96), 0)
 
+
+  /* Sorted? yes? no? how?
+
+  Complete the method which accepts an array of integers, and returns one of the following:
+
+"yes, ascending" - if the numbers in the array are sorted in an ascending order
+"yes, descending" - if the numbers in the array are sorted in a descending order
+"no" - otherwise
+You can assume the array will always be valid, and there will always be one correct answer. */
+
+function isSortedAndHow(array) {
+  return array.every((x, i) => i == 0 || array[i] >= array[i - 1])
+    ? "yes, ascending"
+    : array.every((x, i) => i == 0 || array[i] <= array[i - 1])
+    ? "yes, descending"
+    : "no";
+}
