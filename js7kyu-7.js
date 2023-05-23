eac561815b8a153f6672d0a9668133780f455ad4
Andrew Pedley <apedley2006@gmail.com>
2023-05-23 07:07:10 -0400

    Moves in squared strings (I)

--- js7kyu-7.js
+++ js7kyu-7.js
@@ -160,3 +160,43 @@ return arr
   .map((v, i) => [arr.slice(0, i).join(" "), arr.slice(i).join(" ")])
   .slice(1)
 }
+
+/* Moves in squared strings (I)
+
+This kata is the first of a sequence of four about "Squared Strings".
+
+You are given a string of n lines, each substring being n characters long: For example:
+
+s = "abcd\nefgh\nijkl\nmnop"
+
+We will study some transformations of this square of strings.
+
+Vertical mirror: vert_mirror (or vertMirror or vert-mirror)
+vert_mirror(s) => "dcba\nhgfe\nlkji\nponm"
+Horizontal mirror: hor_mirror (or horMirror or hor-mirror)
+ hor_mirror(s) => "mnop\nijkl\nefgh\nabcd"
+or printed:
+
+vertical mirror   |horizontal mirror   
+abcd --> dcba     |abcd --> mnop 
+efgh     hgfe     |efgh     ijkl 
+ijkl     lkji     |ijkl     efgh 
+mnop     ponm     |mnop     abcd 
+Task:
+Write these two functions
+and
+
+high-order function oper(fct, s) where
+
+fct is the function of one variable f to apply to the string s (fct will be one of vertMirror, horMirror)
+
+Examples:
+s = "abcd\nefgh\nijkl\nmnop"
+oper(vert_mirror, s) => "dcba\nhgfe\nlkji\nponm"
+oper(hor_mirror, s) => "mnop\nijkl\nefgh\nabcd" */
+
+const vertMirror = s => s.map(s => [...s].reverse().join(""))
+const horMirror = s => s.reverse()
+
+const oper = (fct, s) => fct(s.split("\n")).join("\n")
+
