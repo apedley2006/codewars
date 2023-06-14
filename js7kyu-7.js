96417cf6404a43b81a35179012903ce30ffa0264
Andrew Pedley <apedley2006@gmail.com>
2023-06-14 07:13:36 -0400

    Halving Sum

--- js7kyu-7.js
+++ js7kyu-7.js
@@ -492,3 +492,22 @@ function greetDevelopers(list) {
   )
   return list
 }
+
+/* Halving Sum
+
+Given a positive integer n, calculate the following sum:
+
+n + n/2 + n/4 + n/8 + ...
+All elements of the sum are the results of integer division.
+
+Example
+25  =>  25 + 12 + 6 + 3 + 1 = 47 */
+
+function halvingSum(n) {
+  let score = 0
+  while (n >= 1) {
+    score += n
+    n = Math.floor((n * 1) / 2)
+  }
+  return score
+}
