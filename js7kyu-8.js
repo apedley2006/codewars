67abf00511d437ee4231e326d85017dfe60847b0
Andrew Pedley <apedley2006@gmail.com>
2023-07-05 07:25:37 -0400

    Sum of array singles

--- js7kyu-8.js
+++ js7kyu-8.js
@@ -179,3 +179,18 @@ It's your duty to verify that n is a valid positive integer number. If not, plea
 function f(n) {
   return parseInt(n) === n && n > 0 ? (n * (n + 1)) / 2 : false;
 }
+
+/* Sum of array singles
+
+In this Kata, you will be given an array of numbers in which two numbers occur once and the rest occur only twice. Your task will be to return the sum of the numbers that occur only once.
+
+For example, repeats([4,5,7,5,4,8]) = 15 because only the numbers 7 and 8 occur once, and their sum is 15. Every other number occurs twice.
+
+More examples in the test cases. */
+
+function repeats(arr){
+  //..
+    return arr
+      .filter((v, i, arr) => arr.indexOf(v) == arr.lastIndexOf(v))
+      .reduce((a, b) => a + b, 0)
+  };
