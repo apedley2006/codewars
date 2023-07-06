bfeb15d0e898f75e1b3b9d43bce07789e96fef09
Andrew Pedley <apedley2006@gmail.com>
2023-07-06 07:19:06 -0400

    Simple Fun #152: Invite More Women?

--- js7kyu-8.js
+++ js7kyu-8.js
@@ -194,3 +194,28 @@ function repeats(arr){
       .filter((v, i, arr) => arr.indexOf(v) == arr.lastIndexOf(v))
       .reduce((a, b) => a + b, 0)
   };
+
+  /* Simple Fun #152: Invite More Women?
+
+  Task
+King Arthur and his knights are having a New Years party. Last year Lancelot was jealous of Arthur, because Arthur had a date and Lancelot did not, and they started a duel.
+
+To prevent this from happening again, Arthur wants to make sure that there are at least as many women as men at this year's party. He gave you a list of integers of all the party goers.
+
+Arthur needs you to return true if he needs to invite more women or false if he is all set.
+
+Input/Output
+[input] integer array L ($a in PHP)
+An array (guaranteed non-associative in PHP) representing the genders of the attendees, where -1 represents women and 1 represents men.
+
+2 <= L.length <= 50
+
+[output] a boolean value
+
+true if Arthur need to invite more women, false otherwise. */
+
+function inviteMoreWomen(L) {
+  //coding and coding..
+  return L.reduce((a, b) => a + b, 0) > 0;
+}
+
